94fc741f1118ce623ec3b42b6d63a5d816718775
ふらわー <fuwafuwademotimoti@gmail.com>
2026-09-11 23:14:24 +0900

    feat: add category to habit model

--- lib/models/habit.dart
+++ lib/models/habit.dart
@@ -5,33 +5,39 @@ class Habit {
   final String title;
   final IconData icon;
   final List<String> days; //習慣を行う曜日
-  final Map<String, bool> completionHistory; // 日付ごとの達成記録
+  final String category; //ジャンル
+  final Map<String, bool> completionHistory; //日付ごとの達成記録
 
   Habit({
     required this.title,
     required this.icon,
     this.days = const [],
+    this.category = '未設定',
     Map<String, bool>? completionHistory,
   }) : completionHistory = completionHistory ?? {};
 
-  // Habitを保存しやすい形に変換
+  //Habitを保存しやすい形に変換
   Map<String, dynamic> toJson() {
     return {
       'title': title,
       'icon': icon.codePoint,
       'days': days,
+      'category': category,
       'completionHistory': completionHistory,
     };
   }
 
-  // 保存データからHabitを作り直す
+  //保存データからHabitを作り直す
   factory Habit.fromJson(Map<String, dynamic> json) {
     return Habit(
       title: json['title'],
       icon: IconData(json['icon'], fontFamily: 'MaterialIcons'),
-      days: List<String>.from(json['days']),
+      days: List<String>.from(json['days'] ?? []),
 
-      // Map<dynamic, dynamic> を Map<String, bool> に戻す
+      //以前の保存データにはジャンルが無いので未設定にする
+      category: json['category'] ?? '未設定',
+
+      //Map<dynamic, dynamic>をMap<String, bool>に戻す
       completionHistory: Map<String, bool>.from(
         json['completionHistory'] ?? {},
       ),
