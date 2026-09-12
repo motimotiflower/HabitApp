0f3074c2fac63e9cd54558dc8447e24c9ce4741f
ふらわー <fuwafuwademotimoti@gmail.com>
2026-09-12 23:11:47 +0900

    習慣名の文字サイズを拡大

--- lib/z_habit/widgets/habit_card.dart
+++ lib/z_habit/widgets/habit_card.dart
@@ -57,7 +57,7 @@ class HabitCard extends StatelessWidget {
                 Text(
                   habit.title,
                   style: TextStyle(
-                    fontSize: 18,
+                    fontSize: 19,
                     fontWeight: FontWeight.w600,
                     color: const Color(0xff35415F),
                     decoration:
