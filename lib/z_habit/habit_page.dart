a2012e59e2c13d491b5cab613c9210f1c9a2de39
ふらわー <fuwafuwademotimoti@gmail.com>
2026-09-12 23:13:29 +0900

    大画面のBottomSheetを横幅いっぱいに表示

--- lib/z_habit/habit_page.dart
+++ lib/z_habit/habit_page.dart
@@ -70,6 +70,8 @@ class HabitPageState extends State<HabitPage> {
     showModalBottomSheet(
       context: context,
       isScrollControlled: true,
+        //大画面でも追加・編集画面を横幅いっぱいに広げる
+        constraints: const BoxConstraints(maxWidth: double.infinity),
       backgroundColor: Colors.transparent,
       builder: (context) {
         return SizedBox(
@@ -90,6 +92,8 @@ class HabitPageState extends State<HabitPage> {
     showModalBottomSheet(
       context: context,
       isScrollControlled: true,
+        //大画面でも追加・編集画面を横幅いっぱいに広げる
+        constraints: const BoxConstraints(maxWidth: double.infinity),
       backgroundColor: Colors.transparent,
       builder: (context) {
         return SizedBox(
