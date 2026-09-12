ac094ff5f33d47dfb4f6cb0f280aad7135fc2322
ふらわー <fuwafuwademotimoti@gmail.com>
2026-09-13 01:01:53 +0900

    詳細ページのヘッダー高さと上下余白を統一

--- lib/z_memo/memo_composer_page.dart
+++ lib/z_memo/memo_composer_page.dart
@@ -61,6 +61,8 @@ class _MemoComposerPageState extends State<MemoComposerPage> {
     return Scaffold(
       backgroundColor: const Color(0xffF7F9FF),
       appBar: AppBar(
+        //各詳細ページで上下の余白をそろえる
+        toolbarHeight: 52,
         backgroundColor: const Color(0xffF7F9FF),
         elevation: 0,
         leading: IconButton(
