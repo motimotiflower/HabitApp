869a6829e8d51582d9a7cade9963d0beb4aec599
ふらわー <fuwafuwademotimoti@gmail.com>
2026-09-12 23:31:52 +0900

    通知プラグイン向けにAndroid Java 17設定へ更新

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,16 +11,18 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
         isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
+        multiDexEnabled = true
+
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.habitapp"
         // You can update the following values to match your application needs.
