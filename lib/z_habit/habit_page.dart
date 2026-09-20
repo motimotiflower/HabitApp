21e2a277aa88021407bd489d80adfee5ff4ed29a
ふらわー <fuwafuwademotimoti@gmail.com>
2026-09-21 00:46:19 +0900

    Use long press to reorder habits

--- lib/z_habit/habit_page.dart
+++ lib/z_habit/habit_page.dart
@@ -397,6 +397,8 @@ class HabitPageState extends State<HabitPage> {
               // 下に引っ張るとウィジェット側の変更を読み直す
               onRefresh: reloadHabits,
               child: ReorderableListView.builder(
+                    //行を長押しして並び替える
+                    buildDefaultDragHandles: true,
                     physics: const AlwaysScrollableScrollPhysics(),
                     //右下の＋ボタンと最後のチェックが重ならないよう下に余白
                     padding: const EdgeInsets.only(bottom: 88),
@@ -504,15 +506,6 @@ class HabitPageState extends State<HabitPage> {
                             await StarStorage.revoke(actionKey);
                           }
                         },
-                        dragHandle: ReorderableDragStartListener(
-                          index: index,
-                          child: const Padding(
-                            padding: EdgeInsets.all(8),
-                            child: Icon(Icons.drag_handle,
-                                color: Color(0xff81889B)),
-                          ),
-                        ),
-
                         //編集
                         onEdit: () {
                           _showEditSheet(habit);
