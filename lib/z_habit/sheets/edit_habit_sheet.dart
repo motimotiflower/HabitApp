eba299351ebbf6a5c03350f1924a7a8c3fc0631f
ふらわー <fuwafuwademotimoti@gmail.com>
2026-09-22 02:00:09 +0900

    Move habit notification above sharing options

--- lib/z_habit/sheets/edit_habit_sheet.dart
+++ lib/z_habit/sheets/edit_habit_sheet.dart
@@ -369,6 +369,29 @@ class _EditHabitSheetState extends State<EditHabitSheet> {
                       ),
 
                     const SizedBox(height: 18),
+  
+
+                    const SizedBox(height: 18),
+
+                    SwitchListTile(
+                      contentPadding: EdgeInsets.zero,
+                      title: const Text('他の曜日と達成を共有'),
+                      value: _shareCompletion,
+                      onChanged: (value) {
+                        setState(() => _shareCompletion = value);
+                      },
+                    ),
+
+                    SwitchListTile(
+                      contentPadding: EdgeInsets.zero,
+                      title: const Text('やり残しの表示'),
+                      value: _carryOverIfIncomplete,
+                      onChanged: (value) {
+                        setState(() => _carryOverIfIncomplete = value);
+                      },
+                    ),
+
+                    const SizedBox(height: 18),
                       SwitchListTile(
                       contentPadding: EdgeInsets.zero,
                       title: const Text('他の曜日と達成を共有'),
